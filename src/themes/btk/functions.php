52a9df205ac2f1b999373711029fee08b988416e
Benoit Roy <benoit.roy@gmail.com>
2015-02-06 16:59:02 -0500

    basil and e-commerce stuff

--- src/themes/btk/functions.php
+++ src/themes/btk/functions.php
@@ -140,3 +140,6 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+add_theme_support('woocommerce');
+
