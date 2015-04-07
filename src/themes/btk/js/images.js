a559afd7323e1b1f64852ea1e750c16aab8dc14f
franko <franko@akr.club>
2015-04-07 12:19:54 -0400

    requestanimationframe polyfill + use that to wait before replacing srcs

--- src/themes/btk/js/images.js
+++ src/themes/btk/js/images.js
@@ -1,8 +1,13 @@
 ( function( $ ) {
-	$( function(){
+
+	var replaceImages = function(){
 		$('img[data-hires-image]').each( function(){
 			$(this).attr('src', $(this).data( 'hires-image' ) )
 
 		});
+	}
+	$( function(){
+		window.requestAnimationFrame(  replaceImages );
+		
 	})
 })( jQuery )
