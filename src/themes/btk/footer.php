1b963a328cdcc15ac591c5392c3ef0c5ea1d091d
franko <franko@akr.club>
2015-06-25 07:25:39 -0400

    tweaks

--- src/themes/btk/footer.php
+++ src/themes/btk/footer.php
@@ -71,14 +71,14 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//anal.akr.club/";
+    var u="http://anal.akr.club/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', 4]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="//anal.akr.club/piwik.php?idsite=4" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="http://anal.akr.club/piwik.php?idsite=4" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
 </body>
 </html>
