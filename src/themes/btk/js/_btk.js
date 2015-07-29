2938f6dc3e437daad0b26bf34f0028a655066897
franko <franko@akr.club>
2015-07-29 09:50:28 -0400

    desktop woocat menu

--- src/themes/btk/js/_btk.js
+++ src/themes/btk/js/_btk.js
@@ -1,6 +1,7 @@
 /* DO NOT EDIT, THIS FILE IS GENERATED */
 (function($) {
     $(window).load(function() {
+        $('.loading').removeClass('loading');
         // product sub/menu toggles
         $('.woo-categories .toggle').on('click', function() {
             var $it = $(this);
@@ -121,11 +122,7 @@
             slider.toggle(0, 'on');
             $(el).find('.slide').each(function(slide) {
                 $(this).find('img').prependTo($(this).find('.slide-link'))
-            });
-            setTimeout(function() {
-                $('.loading').removeClass('loading');
-            }, 100);
-
+            })
 
         });
 
@@ -233,127 +230,125 @@
         });
 
     })
-})(jQuery);;
-/**
+})(jQuery);;/**
  * Theme Customizer enhancements for a better user experience.
  *
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
-(function($) {
-    var wp = wp || {};
-    if (!wp.customize) return;
-    // Site title and description.
-    wp.customize('blogname', function(value) {
-        value.bind(function(to) {
-            $('.site-title a').text(to);
-        });
-    });
-    wp.customize('blogdescription', function(value) {
-        value.bind(function(to) {
-            $('.site-description').text(to);
-        });
-    });
-    // Header text color.
-    wp.customize('header_textcolor', function(value) {
-        value.bind(function(to) {
-            if ('blank' === to) {
-                $('.site-title, .site-description').css({
-                    'clip': 'rect(1px, 1px, 1px, 1px)',
-                    'position': 'absolute'
-                });
-            } else {
-                $('.site-title, .site-description').css({
-                    'clip': 'auto',
-                    'color': to,
-                    'position': 'relative'
-                });
-            }
-        });
-    });
-})(jQuery);;
-(function($) {
+( function( $ ) {
+        var wp = wp || {};
+        if( !wp.customize ) return;
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title a' ).text( to );
+		} );
+	} );
+	wp.customize( 'blogdescription', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-description' ).text( to );
+		} );
+	} );
+	// Header text color.
+	wp.customize( 'header_textcolor', function( value ) {
+		value.bind( function( to ) {
+			if ( 'blank' === to ) {
+				$( '.site-title, .site-description' ).css( {
+					'clip': 'rect(1px, 1px, 1px, 1px)',
+					'position': 'absolute'
+				} );
+			} else {
+				$( '.site-title, .site-description' ).css( {
+					'clip': 'auto',
+					'color': to,
+					'position': 'relative'
+				} );
+			}
+		} );
+	} );
+} )( jQuery );
+;(function($) {
 
-    var replaceImages = function() {
-        $('img[data-hires-image]').each(function() {
-            var hiressrc = $(this).data('hires-image');
-            if (hiressrc !== $(this).attr('src')) {
-                $(this).attr('src', hiressrc);
-            }
+	var replaceImages = function() {
+		$('img[data-hires-image]').each(function() {
+			var hiressrc = $(this).data('hires-image');
+			if (hiressrc !== $(this).attr('src')) {
+				$(this).attr('src', hiressrc);
+			}
 
 
-        });
-    }
-    $(function() {
-        window.requestAnimationFrame(replaceImages);
+		});
+	}
+	$(function() {
+		window.requestAnimationFrame(replaceImages);
 
-    })
-})(jQuery);
-/**
+	})
+})(jQuery);/**
  * navigation.js
  *
  * Handles toggling the navigation menu for small screens.
  */
+ 
+( function() {
+	var container, button, menu;
 
-(function() {
-    var container, button, menu;
+	container = document.getElementById( 'site-navigation' );
+	if ( ! container ) {
+		return;
+	}
 
-    container = document.getElementById('site-navigation');
-    if (!container) {
-        return;
-    }
+	button = container.getElementsByTagName( 'button' )[0];
+	if ( 'undefined' === typeof button ) {
+		return;
+	}
 
-    button = container.getElementsByTagName('button')[0];
-    if ('undefined' === typeof button) {
-        return;
-    }
+	menu = container.getElementsByTagName( 'ul' )[0];
 
-    menu = container.getElementsByTagName('ul')[0];
+	// Hide menu toggle button if menu is empty and return early.
+	if ( 'undefined' === typeof menu ) {
+		button.style.display = 'none';
+		return;
+	}
 
-    // Hide menu toggle button if menu is empty and return early.
-    if ('undefined' === typeof menu) {
-        button.style.display = 'none';
-        return;
-    }
+	menu.setAttribute( 'aria-expanded', 'false' );
 
-    menu.setAttribute('aria-expanded', 'false');
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
+		menu.className += ' nav-menu';
+	}
 
-    if (-1 === menu.className.indexOf('nav-menu')) {
-        menu.className += ' nav-menu';
-    }
+	button.onclick = function() {
+		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
+			container.className = container.className.replace( ' toggled', '' );
+			button.setAttribute( 'aria-expanded', 'false' );
+			menu.setAttribute( 'aria-expanded', 'false' );
+		} else {
+			container.className += ' toggled';
+			button.setAttribute( 'aria-expanded', 'true' );
+			menu.setAttribute( 'aria-expanded', 'true' );
+		}
+	};
+} )();
+;( function() {
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
 
-    button.onclick = function() {
-        if (-1 !== container.className.indexOf('toggled')) {
-            container.className = container.className.replace(' toggled', '');
-            button.setAttribute('aria-expanded', 'false');
-            menu.setAttribute('aria-expanded', 'false');
-        } else {
-            container.className += ' toggled';
-            button.setAttribute('aria-expanded', 'true');
-            menu.setAttribute('aria-expanded', 'true');
-        }
-    };
-})();;
-(function() {
-    var is_webkit = navigator.userAgent.toLowerCase().indexOf('webkit') > -1,
-        is_opera = navigator.userAgent.toLowerCase().indexOf('opera') > -1,
-        is_ie = navigator.userAgent.toLowerCase().indexOf('msie') > -1;
+	if ( ( is_webkit || is_opera || is_ie ) && document.getElementById && window.addEventListener ) {
+		window.addEventListener( 'hashchange', function() {
+			var element = document.getElementById( location.hash.substring( 1 ) );
 
-    if ((is_webkit || is_opera || is_ie) && document.getElementById && window.addEventListener) {
-        window.addEventListener('hashchange', function() {
-            var element = document.getElementById(location.hash.substring(1));
+			if ( element ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
+					element.tabIndex = -1;
+				}
 
-            if (element) {
-                if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
-                    element.tabIndex = -1;
-                }
-
-                element.focus();
-            }
-        }, false);
-    }
-})();;
-!(function($) {
+				element.focus();
+			}
+		}, false );
+	}
+})();
+;!(function($) {
     // Use basil.js to create an in-browser key-value store.
     var storeOptions = {
         namespace: 'edb-splash',
