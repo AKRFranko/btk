c8b86bc8f90f1123b407efd3eda259cf772d1a78
franko <franko@akr.club>
2015-09-02 19:36:59 -0400

    begin reformating lookbook tempaltes

---
+++ src/themes/btk/category-lookbook.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package buteek
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main lower" role="main">
+      <h1><?php the_title(); ?></h1>
+
+      <?php while ( have_posts() ) : the_post(); ?>
+
+			<div class="lookbook">
+			  <div class="attachment-thumbnail-wrapper">
+				<?php
+					if ( has_post_thumbnail() ) {
+						echo get_the_post_thumbnail();
+					}
+				?>
+				</div>
+				<div class="title">
+          <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+          
+
+					<span class="icon-chevron-lite-right-black"></span>
+				</div>
+			</div>
+
+			<?php endwhile;  ?>
+
+			
+		
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
